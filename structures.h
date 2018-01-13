ccfb552b72b0539b0e9c64c019b24662996cc2e1
Montblac <montblac95@gmail.com>
2018-01-13 14:04:40 -0800

    Added the structures for ReadyList, BlockList, Process Linked List, Resource Linked List. Updated the PCB structure to reflect changes. Removed basic implementation of Linked List.

--- structures.h
+++ structures.h
@@ -5,18 +5,37 @@
 #ifndef CS143B_STRUCTURES_H
 #define CS143B_STRUCTURES_H
 
-typedef struct LinkedList {
-    int data;
-    struct LinkedList * next;
-} Node;
+typedef struct ReadyList {
+    struct PCB * init;
+    struct PCB * user;
+    struct PCB * system;
+} ReadyList;
+
+typedef struct BlockList {
+    struct PCB * r1;
+    struct PCB * r2;
+    struct PCB * r3;
+    struct PCB * r4;
+};
+
+typedef struct ProcessLL {
+    struct PCB * process;
+    struct ProcessLL * next;
+} ProcessNode;
+
+typedef struct ResourceLL {
+    // resource * resource;
+    struct ResourceLL * next;
+} ResourceNode;
 
 typedef struct PCB {
     char pid;	                // Process Name
-    Node resources;	            // Resource Block
+    //Node resources;	            // Linked List of Resource Block
     int status;	                // Status
     struct PCB * parent;	    // Parent
-    //struct PCB * children;	// List of Children
+    struct ProcessLL * child;	// Linked List of Children
     int priority;	            // Priority
 } PCB;
+
 
 #endif //CS143B_STRUCTURES_H
