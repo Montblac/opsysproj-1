7bb27b2b67b721d22ff7f5e8eec515118bcede49
Montblac <montblac95@gmail.com>
2018-01-13 12:18:44 -0800

    Created macros header and defined the three process states.

---
+++ macros.h
@@ -0,0 +1,9 @@
+// Contains macro definitions of the three process states: ready, running, blocked
+#ifndef MACROS_H
+#define MACROS_H
+
+#define READY 1
+#define RUNNING 2
+#define BLOCKED 3
+
+#endif
